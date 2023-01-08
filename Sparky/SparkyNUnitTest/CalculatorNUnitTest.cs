e546fbbb4cf6b5bd60f8793c7331b368f3b62e14
Martin Facorro <mfacorro@live.com.ar>
2023-01-08 11:53:11 -0300

    15. First NUnit Test

---
+++ Sparky/SparkyNUnitTest/CalculatorNUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Sparky
+{
+
+  [TestFixture]
+  public class CalculatorNUnitTest
+  {
+    [Test]
+    public void AddNumbers_InputTwoInt_GetCorrectAddition()
+    {
+      //Arrange
+      Calculator calc = new();
+
+      //Act
+      int result = calc.AddNumber(10, 20);
+
+      //Assert
+      Assert.AreEqual(30, result);
+    }
+  }
+}
