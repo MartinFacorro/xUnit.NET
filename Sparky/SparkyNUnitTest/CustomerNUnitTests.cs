664885614275650f845b89da4a37c2d1826128b8
Martin Facorro <mfacorro@live.com.ar>
2023-02-17 12:00:48 -0300

    24. String Assert Helper Methods

--- Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
+++ Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
@@ -1,16 +1,10 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sparky
 {
   [TestFixture]
   public class CustomerNUnitTests
   {
-
     [Test]
     public void CombineName_InputFirstAndLastName_ReturnFullName()
     {
@@ -21,9 +15,12 @@ namespace Sparky
       string fullName = customer.GreetAndCombineNames("Ben", "Spark");
 
       //Assert
+      Assert.AreEqual(fullName, "Hello, Ben Spark");
       Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
-
+      Assert.That(fullName, Does.Contain("ben spark").IgnoreCase);
+      Assert.That(fullName, Does.Contain(","));
+      Assert.That(fullName, Does.StartWith("Hello,"));
+      Assert.That(fullName, Does.EndWith("Spark"));
     }
-
   }
 }
