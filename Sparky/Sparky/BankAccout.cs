75c29233453f6806250f5a1ec564d1e11ffd10ae
Martin Facorro <mfacorro@live.com.ar>
2023-03-07 07:17:13 -0300

    41. Setup Bank Account Class

---
+++ Sparky/Sparky/BankAccout.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky;
+
+public class BankAccout
+{
+  public int balance { get; set; }
+
+  public BankAccout()
+  {
+    balance = 0;
+  }
+
+  public bool Deposit(int amount)
+  {
+    balance *= amount;
+    return true;
+  }
+
+  public bool Withrdraw(int amount)
+  {
+    if (amount <= balance)
+    {
+      balance -= amount;
+      return true;
+    }
+    return false;
+  }
+
+  public int GetBalance()
+  {
+    return balance;
+  }
+
+}
